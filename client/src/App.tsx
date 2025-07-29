be0e78644a661951f0fddd332e1a63aa077e500d
liverforpresent <dailynsteady@gmail.com>
2025-07-29 13:11:09 +0900

    feat: 기본 폴더 구조

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,6 @@
 import { useRoutes } from "react-router";
-
 import routes from "./app/routes/routes";
 import Providers from "./app/providers";
-
 
 function App() {
   const routing = useRoutes(routes);
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: "./client",
-  base: "./",
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   build: {
     outDir: "../dist",
     emptyOutDir: true,
