833ff65304b41ed8e580782ae22f2bcb7a0b76d3
liverforpresent <dailynsteady@gmail.com>
2025-08-07 14:20:05 +0900

    feat: bottom section 구현

---
+++ client/src/pages/ConfirmPage/components/BottomSection/index.tsx
@@ -0,0 +1,45 @@
+import { TimerDisplay } from "@/shared/components/TimerDisplay";
+import { BackButtonStyle, ButtonBoxStyle, ConfirmButtonStyle, Container, ContentStyle, LeftColumn, PriceAndTimerStyle, RightColumn, TotalPriceLabelStyle, TotalPriceStyle } from "./style.css";
+
+export interface BottomSectionProps {
+  totalPrice: number;
+  timeLeft: number;
+  onConfirm: () => void;
+  onBack: () => void;
+}
+
+const BottomSection = ({ totalPrice, timeLeft, onConfirm, onBack }: BottomSectionProps) => {
+  return (
+    <div className={Container}>
+      <div className={LeftColumn}>
+        <div className={ContentStyle}>
+          <span className={TotalPriceLabelStyle}>총 금액</span>
+          <div className={PriceAndTimerStyle}>
+            <span className={TotalPriceStyle}>
+              ₩{totalPrice.toLocaleString()}
+            </span>
+          </div>
+        </div>
+        <div className={ButtonBoxStyle}>
+          <button
+            onClick={onBack}
+            className={BackButtonStyle}
+          >
+            뒤로가기
+          </button>
+          <button
+            onClick={onConfirm}
+            className={ConfirmButtonStyle}
+          >
+            주문 확인하기
+          </button>
+        </div>
+      </div>
+      <div className={RightColumn}>
+        <TimerDisplay timeLeft={timeLeft} />
+      </div>
+    </div>
+  );
+}
+
+export default BottomSection;
