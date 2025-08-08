3a79b94c29f52a82733e8f7c4269b39018ee325b
liverforpresent <dailynsteady@gmail.com>
2025-08-08 21:56:11 +0900

    feat: success message 구현

---
+++ client/src/pages/CompletionPage/components/SuccessMessage/index.tsx
@@ -0,0 +1,13 @@
+import { Container, SubtitleStyle, ThankYouStyle, TitleStyle } from "./style.css";
+
+const SuccessMessage = () => {
+  return (
+    <div className={Container}>
+      <h1 className={TitleStyle}>주문이 완료되었습니다!</h1>
+      <p className={SubtitleStyle}>아이스크림을 가져가세요</p>
+      <p className={ThankYouStyle}>감사합니다</p>
+    </div>
+  );
+}
+
+export default SuccessMessage;
---
+++ client/src/pages/CompletionPage/components/SuccessMessage/style.css.ts
@@ -0,0 +1,24 @@
+import { css } from "@styled-system/css";
+
+export const Container = css({
+  textAlign: "center",
+  mb: "12",
+});
+
+export const TitleStyle = css({
+  fontSize: "6xl",
+  fontWeight: "bold",
+  color: "green.600",
+  mb: "6",
+});
+
+export const SubtitleStyle = css({
+  fontSize: "3xl",
+  color: "gray.700",
+  mb: "8",
+});
+
+export const ThankYouStyle = css({
+  fontSize: "2xl",
+  color: "gray.600",
+});
