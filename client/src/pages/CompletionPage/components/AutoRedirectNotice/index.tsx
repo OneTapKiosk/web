7cc70a15d2a02cb6bbe55132afd1e46df9fa93b4
liverforpresent <dailynsteady@gmail.com>
2025-08-08 21:57:32 +0900

    feat: auto redirect notice 구현

---
+++ client/src/pages/CompletionPage/components/AutoRedirectNotice/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./style.css";
+
+const AutoRedirectNotice = () => {
+  return (
+    <p className={Container}>
+      10초 후 자동으로 처음 화면으로 돌아갑니다
+    </p>
+  );
+}
+
+export default AutoRedirectNotice;
