ec4e21d8cdbb83b160c6b5383053c5d3de217c55
liverforpresent <dailynsteady@gmail.com>
2025-08-12 19:55:21 +0900

    feat: cart store를 통한 cartid 상태관리 구현 및 적용

---
+++ client/src/shared/store/cartStore.ts
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+interface CartState {
+  cartId: number | null;
+  setCartId: (id: number) => void;
+  resetCartId: () => void;
+}
+
+export const useCartStore = create<CartState>((set) => ({
+  cartId: null,
+  setCartId: (id) => set({ cartId: id }),
+  resetCartId: () => set({ cartId: null }),
+}));
